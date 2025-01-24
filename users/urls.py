a80b7d79729b621fb977d54a7e86dbbd392e3347
Keefe Tan Guan Yu <keefe2307@yahoo.com.sg>
2025-01-24 18:10:42 +0800

    update urls.py
    s

--- users/urls.py
+++ users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserProfileViewSet, UserProfileCreateView
 
+# Initialize the DefaultRouter
 router = DefaultRouter()
 router.register(r'user-profiles', UserProfileViewSet)
 
+# Define urlpatterns
 urlpatterns = [
+    path('', include(router.urls)),  # Include router-generated URLs
     path('profile/user/me', UserProfileCreateView.as_view(), name='create_user_profile'),
 ]
