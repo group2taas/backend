ddd79fe1e1d912d0f06fb3e60c5ad72bcf727f66
LauWyeRock <e0726426@u.nus.edu>
2025-02-10 22:00:48 +0800

    Change model to deepseek OpenRouter

--- agents/base/model_handler.py
+++ agents/base/model_handler.py
@@ -1,12 +1,29 @@
-from huggingface_hub import InferenceClient
+from openai import OpenAI
 import os
+import requests
 
 class AIModelHandler:
     def __init__(self):
-        self.client = InferenceClient(
-            model="codellama/CodeLlama-7b-hf",
-            token=os.getenv("HUGGINGFACEHUB_API_TOKEN")
+        self.api_key = os.getenv("OPENROUTER_API_TOKEN")
+        self.client = OpenAI(
+            base_url="https://openrouter.ai/api/v1",
+            api_key=self.api_key,
         )
-    
-    def query_model(self, prompt, max_new_tokens=100):
-        return self.client.text_generation(prompt, max_new_tokens=max_new_tokens)
+        self.model = "deepseek/deepseek-r1-distill-llama-70b:free"
+
+    def get_usage_info(self):
+        url = "https://openrouter.ai/api/v1/auth/key"
+        response = requests.get(url, headers={
+            "Authorization": f"Bearer {self.api_key}"
+        })
+        return response.json()
+
+    def query_model(self, prompt):
+        completion = self.client.chat.completions.create(
+            extra_body={},
+            model=self.model,
+            messages=[
+                {"role": "user", "content": prompt}
+            ]
+        )
+        return completion
