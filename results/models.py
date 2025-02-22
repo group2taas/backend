0e358ce25a445a7e078e26a01ae9c42fbc4fddfa
Teo Wei Ming <nicholastwm18@gmail.com>
2025-02-22 20:59:51 +0800

    added logs array field to result model and function to save logs

--- results/models.py
+++ results/models.py
@@ -1,6 +1,7 @@
-from django.db import models
+from django.db import models, transaction
 from tickets.models import Ticket
 from django.core.validators import FileExtensionValidator
+from django.core.exceptions import ValidationError
 import os
 
 
@@ -18,7 +19,8 @@ class Result(models.Model):
     title = models.CharField(max_length=255, blank=True, default="")
     created_at = models.DateTimeField(auto_now_add=True)
     ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE, related_name="results")
-    progress = models.IntegerField(default=0)
+    logs = models.JSONField(default=list)
+    progress = models.PositiveIntegerField(default=0)
     pdf = models.FileField(
         upload_to=upload_to,
         blank=False,
@@ -26,10 +28,17 @@ class Result(models.Model):
         validators=[FileExtensionValidator(allowed_extensions=["pdf"])],
     )
 
-    def increment_progress(self):
-        self.progress = models.F("progress") + 1
-        self.save(update_fields=["progress"])
-        self.refresh_from_db()
+    def add_log(self, test_log):
+        with transaction.atomic():
+            result = Result.objects.select_for_update().get(id=self.id)
+            result.logs.append(test_log)
+            result.progress = len(result.logs)
+            result.save(update_fields=["logs", "progress"])
+
+    # def increment_progress(self):
+    #     self.progress = models.F("progress") + 1
+    #     self.save(update_fields=["progress"])
+    #     self.refresh_from_db()
 
     def __str__(self):
         return self.title
