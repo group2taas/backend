af3e92cc43fe87a174a41033c54017278b377566
Teo Wei Ming <nicholastwm18@gmail.com>
2025-02-28 23:22:10 +0800

    added interview and result to ticket serializer

--- tickets/serializers.py
+++ tickets/serializers.py
@@ -1,8 +1,12 @@
 from rest_framework import serializers
 from .models import Ticket
-
+from interviews.serializers import InterviewSerializer
+from results.serializers import ResultSerializer
 
 class TicketSerializer(serializers.ModelSerializer):
+    interview = InterviewSerializer(read_only=True)
+    result =  ResultSerializer(read_only=True)
+    
     class Meta:
         model = Ticket
         fields = "__all__"
