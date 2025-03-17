753b8c99139cdfe03f9e1aa3a2abb65b4cf086db
LauWyeRock <e0726426@u.nus.edu>
2025-03-17 21:44:53 +0800

    Admin Display Results and Users

--- results/admin.py
+++ results/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Result
 
 # Register your models here.
+
+@admin.register(Result)
+class ResultAdmin(admin.ModelAdmin):
+    list_display = ('num_tests', 'logs', 'security_alerts')
+    readonly_fields = ('created_at',)
